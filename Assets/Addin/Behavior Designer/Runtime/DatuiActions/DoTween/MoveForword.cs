08aa77c4eb960b402f52303a1bcb537d0362e3e1
datuihaobai <84951134@qq.com>
2017-09-01 14:02:36 +0800

    小动物ai

---
+++ Assets/Addin/Behavior Designer/Runtime/DatuiActions/DoTween/MoveForword.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using MovementEffects;
+
+[TaskCategory("Datui")]
+public class MoveForword : Action
+{
+    public SharedGameObject targetGameObject;
+    public SharedFloat delay;
+    public SharedFloat distance;
+    public SharedFloat duration;
+    private GameObject prevGameObject;
+    private bool moveComplete;
+
+    public override void OnStart()
+    {
+        var currentGameObject = GetDefaultGameObject(targetGameObject.Value);
+        if (currentGameObject != prevGameObject)
+            prevGameObject = currentGameObject;
+        Timing.RunCoroutine(MoveByCurve());
+    }
+
+    IEnumerator<float> MoveByCurve()
+    {
+        moveComplete = false;
+        yield return Timing.WaitForSeconds(delay.Value);
+        Vector3 startPos = prevGameObject.transform.position;
+        Vector3 endPos = prevGameObject.transform.forward * distance.Value + prevGameObject.transform.position;
+        float normalizedTime = 0.0f;
+        while (normalizedTime < 1.0f)
+        {
+            prevGameObject.transform.position = Vector3.Lerp(startPos, endPos, normalizedTime);
+            normalizedTime += Time.deltaTime / duration.Value;
+            yield return Timing.WaitForOneFrame;
+        }
+        moveComplete = true;
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        if (moveComplete)
+            return TaskStatus.Success;
+        else
+            return TaskStatus.Running;
+    }
+
+    public override void OnReset()
+    {
+        targetGameObject = null;
+        moveComplete = false;
+    }
+}
