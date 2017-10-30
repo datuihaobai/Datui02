dd5a801f6405a41c81ed511871a96041938acb60
datuihaobai <84951134@qq.com>
2017-10-30 14:29:54 +0800

    蛋孵化系统

---
+++ Assets/Scripts/LocalData/EggDataBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggData
+{
+    public enum ElementType
+    {
+        Universal = 0,
+        Fire = 1,
+        Wood = 2,
+    }
+
+    public int id;
+}
+
+public class EggDataBase
+{
+    List<EggData> eggs = new List<EggData>();
+    
+}
---
+++ Assets/Scripts/LocalData/PlayerDataBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+public class PlayerDataBase : SingletonObject<PlayerDataBase> 
+{
+    public int playerId;
+    public EggDataBase eggDataBase = new EggDataBase();
+
+
+
+    public JSONNode ToJson()
+    {
+        JSONNode jsnode = new JSONClass();
+
+        return jsnode;
+    }
+
+    public void FromJson(JSONNode jsnode)
+    {
+
+    }
+}
