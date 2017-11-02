df020015f4c94732a1922ece3ab6176be5e30f88
datuihaobai <84951134@qq.com>
2017-11-02 10:57:05 +0800

    蛋孵化系统 遮罩云

---
+++ Assets/Scripts/Terrain/UI/UIHatch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHatch : MonoBehaviour 
+{
+    public GameObject root;
+
+    public void Show()
+    {
+        root.SetActive(false);
+    }
+
+	public void Hide()
+    {
+        root.SetActive(false);
+    }
+}
